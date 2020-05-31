4bdad8b4bb887ad4a3612c390e50a423f04b7e99
tty <15715715195@163.com>
2020-05-31 19:16:59 +0800

    用HZHControls修改了控件，并增加了修改内存功能

---
+++ MA/MemoryAllocation/Area.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryAllocation
+{
+
+  public  class Area
+    {
+        private string[] states = { "空闲", "忙碌" };
+     public    int ID { set; get; }
+     public    int Start { set; get; }
+     public   int Length { set; get; }
+     public   int State { set; get; }
+        public string States { get => states[State]; }
+    }
+}
