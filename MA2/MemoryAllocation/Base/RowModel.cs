eb28ac5a1268a762be125795724f0240f65c32b9
tty <15715715195@163.com>
2020-06-05 12:53:37 +0800

    修改了字体，使用文件夹归类，加了一些输入限制

---
+++ MA2/MemoryAllocation/Base/RowModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryAllocation
+{
+    class RowModel
+    {
+        public int AreaID { set; get; }
+        public int Start { set; get; }
+        public int Length { set; get; }
+        public string State { set; get; }
+    }
+}
---
+++ MA2/MemoryAllocation/Form/FormEdit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HZH_Controls;
+
+
+namespace MemoryAllocation
+{
+    public partial class FormEdit : Form
+    {
+        public int Modify { set; get; }
+        public FormEdit()
+        {
+            InitializeComponent();
+            this.textBoxEx1.DataBindings.Add("Text", this, "Modify");
+        }
+
+        private void ucBtnExt1_BtnClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
